0075cc4c53041c9a869fae96ce1ed47981af345d
MakeItMike <mbwoodyard@hotmail.com>
2021-04-17 23:28:54 -0400

    Script

---
+++ public/script.js
@@ -0,0 +1,70 @@
+const listContainer = document.querySelector('[data-list]');
+const newListForm   = document.querySelector('[data-new-list-form]');
+const newListInput  = document.querySelector('[data-new-list-input]');
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+const deleteListButton = document.querySelector('[data-delete-list-button]');
+
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
+listContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId;
+        saveAndRender();
+    }
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null;
+    saveAndRender();
+});
+
+
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const listName = newListInput.value;
+    if (listName == null || listName === '') return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    saveAndRender();
+});
+
+function createList(name) {
+   return {id: Date.now().toString(), name: name, tasks: []};
+}
+
+function saveAndRender() {
+    save();
+    render();
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists));
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId);
+}
+
+function render() {
+    clearElement(listContainer);
+    lists.forEach(list => {
+        const listElement = document.createElement('li');
+        listElement.dataset.listId = list.id;
+        listElement.classList.add("list-name");
+        listElement.innerText = list.name;
+        if (list.id === selectedListId) {
+            listElement.classList.add('active-list');
+        }
+        listContainer.appendChild(listElement);
+    });
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+render();
